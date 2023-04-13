d806fe66c1489026c1e5a03b672380694bc80142
qazwsx3134 <qazwsx3134@gmail.com>
2023-04-13 16:05:29 +0800

    config: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3E3B4F",
+        secondary: "#89B0AE",
+        pink: "#F4ACB7",
+        gWhite: "#F7F7FF",
+        white: '#FDFDFD'
+      }
+    },
+  },
+  plugins: [],
+};
