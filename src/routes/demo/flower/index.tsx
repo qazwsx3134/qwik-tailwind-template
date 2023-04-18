f5ca02d5c30025bffc60a59f6b03572782c017ce
qazwsx3134 <qazwsx3134@gmail.com>
2023-04-18 22:30:58 +0800

    init: set up three

--- src/routes/demo/flower/index.tsx
+++ src/routes/demo/flower/index.tsx
@@ -1,10 +1,16 @@
-import { component$, useVisibleTask$, useStore, useStylesScoped$ } from '@builder.io/qwik';
-import { type DocumentHead, useLocation } from '@builder.io/qwik-city';
-import styles from './flower.css?inline';
+import {
+  component$,
+  useVisibleTask$,
+  useStore,
+  useStylesScoped$,
+} from "@builder.io/qwik";
+import { type DocumentHead, useLocation } from "@builder.io/qwik-city";
+import styles from "./flower.css?inline";
 
 export default component$(() => {
   useStylesScoped$(styles);
   const loc = useLocation();
+  console.log(loc.query);
 
   const state = useStore({
     count: 0,
@@ -32,11 +38,11 @@ export default component$(() => {
         />
         <div
           style={{
-            '--state': `${state.count * 0.1}`,
+            "--state": `${state.count * 0.1}`,
           }}
           class={{
             host: true,
-            pride: loc.query.get('pride') === 'true',
+            pride: loc.query.get("pride") === "true",
           }}
         >
           {Array.from({ length: state.number }, (_, i) => (
@@ -46,7 +52,7 @@ export default component$(() => {
                 square: true,
                 odd: i % 2 === 0,
               }}
-              style={{ '--index': `${i + 1}` }}
+              style={{ "--index": `${i + 1}` }}
             />
           )).reverse()}
         </div>
@@ -56,5 +62,5 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: 'Qwik Flower',
+  title: "Qwik Flower",
 };
